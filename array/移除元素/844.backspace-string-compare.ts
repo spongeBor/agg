56593ab6ef59cb51cc6fb036c1bb62930f3aa448
guoyubo <guoyubo@makeblock.com>
2025-04-10 17:35:59 +0800

    feat: 增加一些题目

---
+++ array/移除元素/844.backspace-string-compare.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=844 lang=typescript
+ * @lcpr version=30204
+ *
+ * [844] 比较含退格的字符串
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+function backspaceCompare(s: string, t: string): boolean {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "ab#c"\n"ad#c"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "ab##"\n"c#d#"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "a#c"\n"b"\n
+// @lcpr case=end
+
+ */
+
