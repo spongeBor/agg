72edb729a58c69a1c7daa37d888bc99e3c1149d0
spongeBor <spongeBor@proton.me>
2025-04-10 17:35:59 +0800

    feat: 增加一些题目

---
+++ array/滑动窗口/209.minimum-size-subarray-sum.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=typescript
+ * @lcpr version=30204
+ *
+ * [209] 长度最小的子数组
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+function minSubArrayLen(target: number, nums: number[]): number {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 7\n[2,3,1,2,4,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 4\n[1,4,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 11\n[1,1,1,1,1,1,1,1]\n
+// @lcpr case=end
+
+ */
+
---
+++ array/移除元素/977.squares-of-a-sorted-array.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=977 lang=typescript
+ * @lcpr version=30204
+ *
+ * [977] 有序数组的平方
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+function sortedSquares(nums: number[]): number[] {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [-4,-1,0,3,10]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [-7,-3,2,3,11]\n
+// @lcpr case=end
+
+ */
+
