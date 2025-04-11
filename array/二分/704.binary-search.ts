a42f2244b3e2c4cde137510dfa6974ad45ea6913
spongeBor <spongeBor@proton.me>
2025-04-11 15:13:17 +0800

    feat: add 704

--- array/二分/704.binary-search.ts
+++ array/二分/704.binary-search.ts
@@ -11,7 +11,18 @@
 // @lcpr-template-end
 // @lc code=start
 function search(nums: number[], target: number): number {
-    
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+        const mid = left + ~~(right - left) / 2;
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
 };
 // @lc code=end
 
