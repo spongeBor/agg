90df435c3a0f2e0afb9480f1b7b7dcd81dd2f47c
spongeBor <spongeBor@proton.me>
2025-04-14 15:06:17 +0800

    feat: add 977

---
+++ array/移除元素/977.有序数组的平方.ts
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=977 lang=typescript
+ *
+ * [977] 有序数组的平方
+ *
+ * https://leetcode.cn/problems/squares-of-a-sorted-array/description/
+ *
+ * algorithms
+ * Easy (68.99%)
+ * Likes:    1095
+ * Dislikes: 0
+ * Total Accepted:    833.8K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[-4,-1,0,3,10]'
+ *
+ * 给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+ *
+ *
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [-4,-1,0,3,10]
+ * 输出：[0,1,9,16,100]
+ * 解释：平方后，数组变为 [16,1,0,9,100]
+ * 排序后，数组变为 [0,1,9,16,100]
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [-7,-3,2,3,11]
+ * 输出：[4,9,9,49,121]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums 已按 非递减顺序 排序
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ *
+ * 请你设计时间复杂度为 O(n) 的算法解决本问题
+ *
+ *
+ */
+
+// @lc code=start
+function sortedSquares(nums: number[]): number[] {
+  const ans: number[] = [];
+  let left = 0,
+    right = nums.length - 1;
+  // 非递减序列，右侧值可以不取绝对值
+  while (left <= right) {
+    if (Math.abs(nums[left]) > nums[right]) {
+      ans.unshift(nums[left] ** 2);
+      left++;
+    } else {
+      ans.unshift(nums[right] ** 2);
+      right--;
+    }
+  }
+  return ans;
+}
+// @lc code=end
