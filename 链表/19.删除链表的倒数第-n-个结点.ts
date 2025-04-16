821d6062246b60aa06970c8c730083b8924dbde2
spongeB <spongeBor@proton.me>
2025-04-16 23:23:26 +0800

    feat: add 19

--- 链表/19.删除链表的倒数第-n-个结点.ts
+++ 链表/19.删除链表的倒数第-n-个结点.ts
@@ -14,45 +14,45 @@
  * Testcase Example:  '[1,2,3,4,5]\n2'
  *
  * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：head = [1,2,3,4,5], n = 2
  * 输出：[1,2,3,5]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：head = [1], n = 1
  * 输出：[]
- * 
- * 
+ *
+ *
  * 示例 3：
- * 
- * 
+ *
+ *
  * 输入：head = [1,2], n = 1
  * 输出：[1]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 链表中结点的数目为 sz
  * 1 <= sz <= 30
  * 0 <= Node.val <= 100
  * 1 <= n <= sz
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 进阶：你能尝试使用一趟扫描实现吗？
- * 
+ *
  */
 
 // @lc code=start
@@ -69,7 +69,18 @@
  */
 
 function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
-    
-};
+  const dummyHead = new ListNode(-1, head);
+  let fast = dummyHead;
+  let slow = dummyHead;
+  let i = 0;
+  while (n--) {
+    fast = fast.next;
+  }
+  while (fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  slow.next = slow.next.next;
+  return dummyHead.next;
+}
 // @lc code=end
-
