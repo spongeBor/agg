55001f2cc9ee4e6c2003ab949188a0dbcf74da7f
spongeB <spongeBor@proton.me>
2025-04-16 22:33:36 +0800

    feat: finished 206

--- 链表/206.反转链表.ts
+++ 链表/206.反转链表.ts
@@ -14,46 +14,46 @@
  * Testcase Example:  '[1,2,3,4,5]'
  *
  * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：head = [1,2,3,4,5]
  * 输出：[5,4,3,2,1]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：head = [1,2]
  * 输出：[2,1]
- * 
- * 
+ *
+ *
  * 示例 3：
- * 
- * 
+ *
+ *
  * 输入：head = []
  * 输出：[]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 链表中节点的数目范围是 [0, 5000]
- * -5000 
- * 
- * 
- * 
- * 
+ * -5000
+ *
+ *
+ *
+ *
  * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
- * 
- * 
- * 
+ *
+ *
+ *
  */
 
 // @lc code=start
@@ -70,7 +70,30 @@
  */
 
 function reverseList(head: ListNode | null): ListNode | null {
-    
-};
-// @lc code=end
+  //   return reverseListIterative(head);
+  return reverseListRecursive(head);
+}
 
+// 迭代法
+function reverseListIterative(head: ListNode | null): ListNode | null {
+  let prev = null;
+  let current = head;
+  while (current) {
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+}
+
+// 递归法
+function reverseListRecursive(head: ListNode | null): ListNode | null {
+  if (!head || !head.next) return head;
+  const last = reverseListRecursive(head.next);
+  head.next.next = head;
+  head.next = null;
+  return last;
+}
+
+// @lc code=end
