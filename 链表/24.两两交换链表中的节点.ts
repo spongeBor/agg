250f2d9e1f46b9d7cfe5d22181650ae6d57149ce
spongeB <spongeBor@proton.me>
2025-04-16 23:03:11 +0800

    feat: add 24

--- 链表/24.两两交换链表中的节点.ts
+++ 链表/24.两两交换链表中的节点.ts
@@ -14,39 +14,39 @@
  * Testcase Example:  '[1,2,3,4]'
  *
  * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：head = [1,2,3,4]
  * 输出：[2,1,4,3]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：head = []
  * 输出：[]
- * 
- * 
+ *
+ *
  * 示例 3：
- * 
- * 
+ *
+ *
  * 输入：head = [1]
  * 输出：[1]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 链表中节点的数目在范围 [0, 100] 内
  * 0 <= Node.val <= 100
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -63,7 +63,16 @@
  */
 
 function swapPairs(head: ListNode | null): ListNode | null {
-    
-};
+  const dummy = new ListNode(-1, head);
+  let curr = dummy;
+  while (curr && curr.next && curr.next.next) {
+    const node1 = curr.next;
+    const node2 = curr.next.next;
+    curr.next = node2;
+    node1.next = node2.next;
+    node2.next = node1;
+    curr = node1;
+  }
+  return dummy.next;
+}
 // @lc code=end
-
