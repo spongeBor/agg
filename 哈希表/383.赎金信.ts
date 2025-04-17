c4d7937f56e15c27adcc4ebb89b1d2988fefe508
spongeBor <spongeBor@proton.me>
2025-04-17 12:50:01 +0800

    feat: 增加哈希表

---
+++ 哈希表/383.赎金信.ts
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=383 lang=typescript
+ *
+ * [383] 赎金信
+ *
+ * https://leetcode.cn/problems/ransom-note/description/
+ *
+ * algorithms
+ * Easy (66.94%)
+ * Likes:    965
+ * Dislikes: 0
+ * Total Accepted:    618K
+ * Total Submissions: 923K
+ * Testcase Example:  '"a"\n"b"'
+ *
+ * 给你两个字符串：ransomNote 和 magazine ，判断 ransomNote 能不能由 magazine 里面的字符构成。
+ * 
+ * 如果可以，返回 true ；否则返回 false 。
+ * 
+ * magazine 中的每个字符只能在 ransomNote 中使用一次。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：ransomNote = "a", magazine = "b"
+ * 输出：false
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：ransomNote = "aa", magazine = "ab"
+ * 输出：false
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：ransomNote = "aa", magazine = "aab"
+ * 输出：true
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= ransomNote.length, magazine.length <= 10^5
+ * ransomNote 和 magazine 由小写英文字母组成
+ * 
+ * 
+ */
+
+// @lc code=start
+function canConstruct(ransomNote: string, magazine: string): boolean {
+    
+};
+// @lc code=end
+
---
+++ 哈希表/438.找到字符串中所有字母异位词.ts
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=438 lang=typescript
+ *
+ * [438] 找到字符串中所有字母异位词
+ *
+ * https://leetcode.cn/problems/find-all-anagrams-in-a-string/description/
+ *
+ * algorithms
+ * Medium (54.06%)
+ * Likes:    1656
+ * Dislikes: 0
+ * Total Accepted:    664.5K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"cbaebabacd"\n"abc"'
+ *
+ * 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: s = "cbaebabacd", p = "abc"
+ * 输出: [0,6]
+ * 解释:
+ * 起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+ * 起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: s = "abab", p = "ab"
+ * 输出: [0,1,2]
+ * 解释:
+ * 起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+ * 起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+ * 起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 1 <= s.length, p.length <= 3 * 10^4
+ * s 和 p 仅包含小写字母
+ * 
+ * 
+ */
+
+// @lc code=start
+function findAnagrams(s: string, p: string): number[] {
+    
+};
+// @lc code=end
+
