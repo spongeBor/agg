c789dbdac57eda56591c149c59804598c9254008
spongeB <spongeBor@proton.me>
2025-04-18 00:10:09 +0800

    feat: finished 202

--- 哈希表/202.快乐数.ts
+++ 哈希表/202.快乐数.ts
@@ -14,22 +14,22 @@
  * Testcase Example:  '19'
  *
  * 编写一个算法来判断一个数 n 是不是快乐数。
- * 
+ *
  * 「快乐数」 定义为：
- * 
- * 
+ *
+ *
  * 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
  * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
  * 如果这个过程 结果为 1，那么这个数就是快乐数。
- * 
- * 
+ *
+ *
  * 如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：n = 19
  * 输出：true
  * 解释：
@@ -37,28 +37,39 @@
  * 8^2 + 2^2 = 68
  * 6^2 + 8^2 = 100
  * 1^2 + 0^2 + 0^2 = 1
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：n = 2
  * 输出：false
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 1 <= n <= 2^31 - 1
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
 function isHappy(n: number): boolean {
-    
-};
+  const getNext = (n: number) => {
+    return String(n)
+      .split("")
+      .reduce((acc, cur) => {
+        return acc + Math.pow(Number(cur), 2);
+      }, 0);
+  };
+  const set = new Set();
+  while (n !== 1 && !set.has(n)) {
+    set.add(n);
+    n = getNext(n);
+  }
+  return n === 1;
+}
 // @lc code=end
-
