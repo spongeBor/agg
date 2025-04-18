af642b06f22281d705fdfa7273988eaa338b9ee9
spongeBor <spongeBor@proton.me>
2025-04-18 15:52:19 +0800

    feat: finished 454

--- 哈希表/454.四数相加-ii.ts
+++ 哈希表/454.四数相加-ii.ts
@@ -15,17 +15,17 @@
  *
  * 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l)
  * 能满足：
- * 
- * 
+ *
+ *
  * 0 <= i, j, k, l < n
  * nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
  * 输出：2
  * 解释：
@@ -34,33 +34,51 @@
  * (-1) + 2 = 0
  * 2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) +
  * (-1) + 0 = 0
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
  * 输出：1
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * n == nums1.length
  * n == nums2.length
  * n == nums3.length
  * n == nums4.length
  * 1 <= n <= 200
  * -2^28 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 2^28
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
-function fourSumCount(nums1: number[], nums2: number[], nums3: number[], nums4: number[]): number {
-    
-};
+function fourSumCount(
+  nums1: number[],
+  nums2: number[],
+  nums3: number[],
+  nums4: number[]
+): number {
+  const map = new Map<number, number>();
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      const sum = nums1[i] + nums2[j];
+      map.set(sum, (map.get(sum) || 0) + 1);
+    }
+  }
+  let count = 0;
+  for (let i = 0; i < nums3.length; i++) {
+    for (let j = 0; j < nums4.length; j++) {
+      const sum = nums3[i] + nums4[j];
+      count += map.get(-sum) || 0;
+    }
+  }
+  return count;
+}
 // @lc code=end
-
