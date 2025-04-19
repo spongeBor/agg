c36fd06f5efcbe0698bdeb3dd707c88881d73df8
spongeB <spongeBor@proton.me>
2025-04-19 09:58:38 +0800

    feat: finished 18

--- 哈希表/18.四数之和.ts
+++ 哈希表/18.四数之和.ts
@@ -15,46 +15,75 @@
  *
  * 给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a],
  * nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
- * 
- * 
+ *
+ *
  * 0 <= a, b, c, d < n
  * a、b、c 和 d 互不相同
  * nums[a] + nums[b] + nums[c] + nums[d] == target
- * 
- * 
+ *
+ *
  * 你可以按 任意顺序 返回答案 。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：nums = [1,0,-1,0,-2,2], target = 0
  * 输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：nums = [2,2,2,2,2], target = 8
  * 输出：[[2,2,2,2]]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 1 <= nums.length <= 200
  * -10^9 <= nums[i] <= 10^9
  * -10^9 <= target <= 10^9
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
 function fourSum(nums: number[], target: number): number[][] {
-    
-};
+  // 1. 排序
+  // 2. 双指针
+  // 3. 去重
+  // 4. 返回结果
+  const result: number[][] = [];
+  const len = nums.length;
+  if (len < 4) return result;
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < len - 3; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    for (let j = i + 1; j < len - 2; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+      let left = j + 1;
+      let right = len - 1;
+      while (left < right) {
+        const sum = nums[i] + nums[j] + nums[left] + nums[right];
+        if (sum === target) {
+          result.push([nums[i], nums[j], nums[left], nums[right]]);
+          while (left < right && nums[left] === nums[left + 1]) left++;
+          while (left < right && nums[right] === nums[right - 1]) right--;
+          left++;
+          right--;
+        } else if (sum < target) {
+          left++;
+        } else {
+          right--;
+        }
+      }
+    }
+  }
+  return result;
+}
 // @lc code=end
-
