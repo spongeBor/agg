5ad1070f3de04aec1aee175a41a39578d86d77d5
spongeBor <spongeBor@proton.me>
2025-04-21 10:05:23 +0800

    feat: add 1047

--- 栈与队列/1047.删除字符串中的所有相邻重复项.ts
+++ 栈与队列/1047.删除字符串中的所有相邻重复项.ts
@@ -7,11 +7,19 @@
 
 // @lc code=start
 function removeDuplicates(s: string): string {
-    
-};
+  const stack: string[] = [];
+  for (let i = 0; i < s.length; i++) {
+    // 如果栈不为空且栈顶元素与当前字符相同，则弹出栈顶元素（相邻重复项抵消）
+    if (stack.length > 0 && stack[stack.length - 1] === s[i]) {
+      stack.pop();
+    } else {
+      // 否则将当前字符入栈
+      stack.push(s[i]);
+    }
+  }
+  return stack.join("");
+}
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -19,4 +27,3 @@ function removeDuplicates(s: string): string {
 // @lcpr case=end
 
  */
-
