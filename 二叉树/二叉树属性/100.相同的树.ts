18d814e3fad316060a3a498f5d47675bbc0c1b09
spongeB <spongeBor@proton.me>
2025-04-24 06:03:34 +0800

    feat: 增加100和572，这两个都是101相似题目

---
+++ 二叉树/二叉树属性/100.相同的树.ts
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=typescript
+ * @lcpr version=30104
+ *
+ * [100] 相同的树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3]\n[1,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n[1,null,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,1]\n[1,1,2]\n
+// @lcpr case=end
+
+ */
+
---
+++ 二叉树/二叉树属性/572.另一棵树的子树.ts
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=572 lang=typescript
+ * @lcpr version=30104
+ *
+ * [572] 另一棵树的子树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,4,5,1,2]\n[4,1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,4,5,1,2,null,null,null,null,0]\n[4,1,2]\n
+// @lcpr case=end
+
+ */
+
