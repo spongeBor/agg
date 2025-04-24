1416a0164a67bf1c42a4fa18acd5e35dcbb517b2
spongeBor <spongeBor@proton.me>
2025-04-24 18:31:23 +0800

    feat: finished 112

--- 二叉树/二叉树属性/112.路径总和.ts
+++ 二叉树/二叉树属性/112.路径总和.ts
@@ -22,11 +22,41 @@ export default {};
  */
 
 function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
-    
-};
+  // return recursiveSample(root, targetSum);
+  if (!root) return false;
+  return recursive(root, targetSum - root.val);
+}
+
+function recursive(root: TreeNode | null, targetSum: number): boolean {
+  if (!root) return false; // 空节点
+  if (!root.left && !root.right && targetSum === 0) return true; // 叶子节点且路径和为0
+  if (!root.left && !root.right) return false; // 叶子节点且路径和不为0
+  if (root.left) {
+    targetSum -= root.left.val; // 减去左子节点的值
+    if (recursive(root.left, targetSum)) return true; // 递归判断左子节点
+    targetSum += root.left.val; // 回溯
+  }
+  if (root.right) {
+    targetSum -= root.right.val; // 减去右子节点的值
+    if (recursive(root.right, targetSum)) return true; // 递归判断右子节点
+    targetSum += root.right.val; // 回溯
+  }
+  return false;
+}
+
+function recursiveSample(root: TreeNode | null, targetSum: number): boolean {
+  if (!root) return false;
+  if (!root.left && !root.right) return root.val === targetSum;
+  return (
+    recursiveSample(root.left, targetSum - root.val) ||
+    recursiveSample(root.right, targetSum - root.val)
+  );
+}
+
+function iterative(root: TreeNode | null, targetSum: number): boolean {
+  // TODO:
+}
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -42,4 +72,3 @@ function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
 // @lcpr case=end
 
  */
-
---
+++ 二叉树/二叉树属性/113.路径总和 II.ts
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=113 lang=typescript
+ * @lcpr version=30200
+ *
+ * [113] 路径总和 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function pathSum(root: TreeNode | null, targetSum: number): number[][] {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [5,4,8,11,null,13,4,7,2,null,null,5,1]\n22\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3]\n5\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n0\n
+// @lcpr case=end
+
+ */
+
