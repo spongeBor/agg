e3705de76d4e62c24fba332c70e8e4f885b421a1
spongeB <spongeBor@proton.me>
2025-04-26 10:44:50 +0800

    feat: finished 617

--- 二叉树/二叉树修改与构造/617.合并二叉树.ts
+++ 二叉树/二叉树修改与构造/617.合并二叉树.ts
@@ -21,12 +21,56 @@ export default {};
  * }
  */
 
-function mergeTrees(root1: TreeNode | null, root2: TreeNode | null): TreeNode | null {
-    
-};
+function mergeTrees(
+  root1: TreeNode | null,
+  root2: TreeNode | null
+): TreeNode | null {
+  //   return recursiveMerge(root1, root2);
+  return iterativeMerge(root1, root2);
+}
+function recursiveMerge(
+  root1: TreeNode | null,
+  root2: TreeNode | null
+): TreeNode | null {
+  if (!root1) return root2;
+  if (!root2) return root1;
+  root1.val += root2.val;
+  root1.left = recursiveMerge(root1.left, root2.left);
+  root1.right = recursiveMerge(root1.right, root2.right);
+  return root1;
+}
+
+function iterativeMerge(
+  root1: TreeNode | null,
+  root2: TreeNode | null
+): TreeNode | null {
+  if (!root1) return root2;
+  if (!root2) return root1;
+  const queue: TreeNode[] = [];
+  queue.push(root1);
+  queue.push(root2);
+  while (queue.length > 0) {
+    const node1 = queue.shift();
+    const node2 = queue.shift();
+    node1.val += node2.val;
+    if (node1.left && node2.left) {
+      queue.push(node1.left);
+      queue.push(node2.left);
+    }
+    if (node1.right && node2.right) {
+      queue.push(node1.right);
+      queue.push(node2.right);
+    }
+    if (!node1.left && node2.left) {
+      node1.left = node2.left;
+    }
+    if (!node1.right && node2.right) {
+      node1.right = node2.right;
+    }
+  }
+  return root1;
+}
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -38,4 +82,3 @@ function mergeTrees(root1: TreeNode | null, root2: TreeNode | null): TreeNode | 
 // @lcpr case=end
 
  */
-
