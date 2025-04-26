5405551c0217de3eeb03d562a7668fa3c78ff3b8
spongeB <spongeBor@proton.me>
2025-04-26 20:07:36 +0800

    feat: finished 98

--- 二叉树/求二叉搜索数的属性/98.验证二叉搜索树.ts
+++ 二叉树/求二叉搜索数的属性/98.验证二叉搜索树.ts
@@ -22,11 +22,57 @@ export default {};
  */
 
 function isValidBST(root: TreeNode | null): boolean {
-    
-};
+  // return recursiveIsValidBST(root);
+  return iterativeIsValidBST(root);
+}
+function recursiveIsValidBST(
+  root: TreeNode | null,
+  min: number = -Infinity,
+  max: number = Infinity
+): boolean {
+  if (!root) return true;
+  // 当前节点值必须在有效范围内
+  if (root.val <= min || root.val >= max) return false;
+  // 左子树的所有节点值必须小于当前节点值，右子树的所有节点值必须大于当前节点值
+  return (
+    recursiveIsValidBST(root.left, min, root.val) &&
+    recursiveIsValidBST(root.right, root.val, max)
+  );
+}
+
+function iterativeIsValidBST(root: TreeNode | null): boolean {
+  if (!root) return true;
+
+  const stack: TreeNode[] = [];
+  let prev: number = -Infinity;
+  let curr: TreeNode | null = root;
+
+  // 中序遍历二叉搜索树应该得到一个递增序列
+  while (curr || stack.length > 0) {
+    // 遍历到最左节点
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+
+    // 处理当前节点
+    curr = stack.pop()!;
+
+    // 如果当前节点值小于等于前一个值，则不是BST
+    if (curr.val <= prev) {
+      return false;
+    }
+
+    // 更新前一个值
+    prev = curr.val;
+
+    // 处理右子树
+    curr = curr.right;
+  }
+
+  return true;
+}
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -38,4 +84,3 @@ function isValidBST(root: TreeNode | null): boolean {
 // @lcpr case=end
 
  */
-
