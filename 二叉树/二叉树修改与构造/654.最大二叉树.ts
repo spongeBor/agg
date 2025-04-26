1051bf46f2c51e1e9c83cad31867a5c3ef85d709
spongeB <spongeBor@proton.me>
2025-04-26 10:26:28 +0800

    feat: finished 654

--- 二叉树/二叉树修改与构造/654.最大二叉树.ts
+++ 二叉树/二叉树修改与构造/654.最大二叉树.ts
@@ -26,6 +26,30 @@ function constructMaximumBinaryTree(nums: number[]): TreeNode | null {
   // 1. 根节点是数组中最大的数
   // 2. 左子树是通过数组中最大值左边部分构造出的最大二叉树。
   // 3. 右子树是通过数组中最大值右边部分构造出的最大二叉树。
+
+  // 如果数组为空，返回null
+  if (nums.length === 0) {
+    return null;
+  }
+
+  // 找到数组中的最大值及其索引
+  let maxVal = nums[0];
+  let maxIndex = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > maxVal) {
+      maxVal = nums[i];
+      maxIndex = i;
+    }
+  }
+
+  // 创建根节点
+  const root = new TreeNode(maxVal);
+
+  // 递归构建左子树和右子树
+  root.left = constructMaximumBinaryTree(nums.slice(0, maxIndex));
+  root.right = constructMaximumBinaryTree(nums.slice(maxIndex + 1));
+
+  return root;
 }
 // @lc code=end
 
