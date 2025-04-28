671724e13853996d4b18e793df541b9bf7390efb
spongeBor <spongeBor@proton.me>
2025-04-28 16:14:22 +0800

    feat: finished 669

--- 二叉树/二叉搜索树的修改与构造/669.修剪二叉搜索树.ts
+++ 二叉树/二叉搜索树的修改与构造/669.修剪二叉搜索树.ts
@@ -21,12 +21,64 @@ export default {};
  * }
  */
 
-function trimBST(root: TreeNode | null, low: number, high: number): TreeNode | null {
-    
-};
+function trimBST(
+  root: TreeNode | null,
+  low: number,
+  high: number
+): TreeNode | null {
+  // return recursive(root, low, high);
+  return iterative(root, low, high);
+}
+
+function recursive(
+  root: TreeNode | null,
+  low: number,
+  high: number
+): TreeNode | null {
+  if (root === null) return null;
+  if (root.val < low) {
+    return recursive(root.right, low, high); // 如果当前节点值小于low，则只保留右子树
+  } else if (root.val > high) {
+    return recursive(root.left, low, high); // 如果当前节点值大于high，则只保留左子树
+  } else {
+    root.left = recursive(root.left, low, high); // 接收左子树的返回值
+    root.right = recursive(root.right, low, high); // 接收右子树的返回值
+    return root;
+  }
+}
+
+// 迭代法
+function iterative(
+  root: TreeNode | null,
+  low: number,
+  high: number
+): TreeNode | null {
+  if (!root) return null;
+  // 处理头结点，让root移动到[L, R] 范围内，注意是左闭右开
+  while (root !== null && (root.val < low || root.val > high)) {
+    if (root.val < low) root = root.right;
+    else root = root.left;
+  }
+  // 此时root已经在[L, R] 范围内，处理左孩子元素小于L的情况
+  let cur = root;
+  while (cur !== null) {
+    while (cur.left !== null && cur.left.val < low) {
+      cur.left = cur.left.right;
+    }
+    cur = cur.left;
+  }
+  // 此时root已经在[L, R] 范围内，处理右孩子元素大于R的情况
+  cur = root;
+  while (cur !== null) {
+    while (cur.right !== null && cur.right.val > high) {
+      cur.right = cur.right.left;
+    }
+    cur = cur.right;
+  }
+  return root;
+}
+
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -38,4 +90,3 @@ function trimBST(root: TreeNode | null, low: number, high: number): TreeNode | n
 // @lcpr case=end
 
  */
-
