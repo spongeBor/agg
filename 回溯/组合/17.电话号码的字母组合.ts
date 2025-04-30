4123d40efd2c6561ba3bd36b8d7242339d74690a
spongeB <spongeBor@proton.me>
2025-04-30 21:19:50 +0800

    feat: add 17

--- 回溯/组合/17.电话号码的字母组合.ts
+++ 回溯/组合/17.电话号码的字母组合.ts
@@ -8,11 +8,36 @@ export default {};
 
 // @lc code=start
 function letterCombinations(digits: string): string[] {
-    
-};
+  if (digits.length === 0) return [];
+  const result: string[] = [];
+  const path: string[] = [];
+  const letterMap = {
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"],
+  };
+  const dfs = (index: number) => {
+    if (index === digits.length) {
+      result.push(path.join(""));
+      return;
+    }
+    const digit = digits[index];
+    const letters = letterMap[digit];
+    for (const letter of letters) {
+      path.push(letter);
+      dfs(index + 1);
+      path.pop();
+    }
+  };
+  dfs(0);
+  return result;
+}
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -28,4 +53,3 @@ function letterCombinations(digits: string): string[] {
 // @lcpr case=end
 
  */
-
