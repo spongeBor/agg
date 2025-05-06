9327f685b8aa3203827a0cb8f063555daf98314c
spongeBor <spongeBor@proton.me>
2025-05-06 17:37:53 +0800

    add 332,51,37

---
+++ 回溯/332.重新安排行程.ts
@@ -0,0 +1,27 @@
+export default {};
+/*
+ * @lc app=leetcode.cn id=332 lang=typescript
+ * @lcpr version=30200
+ *
+ * [332] 重新安排行程
+ */
+
+// @lc code=start
+function findItinerary(tickets: string[][]): string[] {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]\n
+// @lcpr case=end
+
+ */
+
---
+++ 回溯/棋盘问题/51.N 皇后.ts
@@ -0,0 +1,27 @@
+export default {};
+/*
+ * @lc app=leetcode.cn id=51 lang=typescript
+ * @lcpr version=30200
+ *
+ * [51] N 皇后
+ */
+
+// @lc code=start
+function solveNQueens(n: number): string[][] {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 4\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 1\n
+// @lcpr case=end
+
+ */
+
