06230bbf9cddc994ec5b30088e24d8ea2fad3df2
spongeBor <spongeBor@proton.me>
2025-05-06 17:25:26 +0800

    feat: add 46, 47

---
+++ 回溯/排列/46.全排列.ts
@@ -0,0 +1,31 @@
+export default {};
+/*
+ * @lc app=leetcode.cn id=46 lang=typescript
+ * @lcpr version=30200
+ *
+ * [46] 全排列
+ */
+
+// @lc code=start
+function permute(nums: number[]): number[][] {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+
---
+++ 回溯/排列/47.全排列 II.ts
@@ -0,0 +1,27 @@
+export default {};
+/*
+ * @lc app=leetcode.cn id=47 lang=typescript
+ * @lcpr version=30200
+ *
+ * [47] 全排列 II
+ */
+
+// @lc code=start
+function permuteUnique(nums: number[]): number[][] {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3]\n
+// @lcpr case=end
+
+ */
+
