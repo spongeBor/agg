ea63c3948a5bee1d672e67195735c534fce375bc
spongeBor <spongeBor@proton.me>
2025-05-06 17:51:01 +0800

    feat: add 47

--- 回溯/排列/47.全排列 II.ts
+++ 回溯/排列/47.全排列 II.ts
@@ -8,11 +8,36 @@ export default {};
 
 // @lc code=start
 function permuteUnique(nums: number[]): number[][] {
-    
-};
+  // 时间复杂度：O(n!)
+  // 空间复杂度：O(n)
+  const result: number[][] = [];
+  const path: number[] = [];
+  const used: boolean[] = new Array(nums.length).fill(false);
+  nums.sort((a, b) => a - b); // 排序，为了方便去重
+
+  function backtracking() {
+    if (path.length === nums.length) {
+      result.push([...path]);
+      return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      // 去重逻辑：如果当前元素与前一个元素相同，并且前一个元素未被使用，则跳过
+      if (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) continue;
+
+      if (used[i]) continue;
+      used[i] = true;
+      path.push(nums[i]);
+      backtracking();
+      path.pop();
+      used[i] = false;
+    }
+  }
+
+  backtracking();
+  return result;
+}
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -24,4 +49,3 @@ function permuteUnique(nums: number[]): number[][] {
 // @lcpr case=end
 
  */
-
