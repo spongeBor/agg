44892861c6e73a50d5a131aa1353e07ca54e14ae
spongeBor <spongeBor@proton.me>
2025-05-06 17:14:55 +0800

    feat: finished 90

--- 回溯/子集/90.子集 II.ts
+++ 回溯/子集/90.子集 II.ts
@@ -1,3 +1,4 @@
+export default {};
 /*
  * @lc app=leetcode.cn id=90 lang=typescript
  * @lcpr version=30200
@@ -7,11 +8,29 @@
 
 // @lc code=start
 function subsetsWithDup(nums: number[]): number[][] {
-    
-};
+  const result: number[][] = [];
+  const path: number[] = [];
+  // 先排序，让相同的元素相邻
+  nums.sort((a, b) => a - b);
+
+  function backtracking(start: number) {
+    result.push([...path]);
+
+    for (let i = start; i < nums.length; i++) {
+      // 跳过同一层级的重复元素
+      if (i > start && nums[i] === nums[i - 1]) {
+        continue;
+      }
+      path.push(nums[i]);
+      backtracking(i + 1);
+      path.pop();
+    }
+  }
+
+  backtracking(0);
+  return result;
+}
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -23,4 +42,3 @@ function subsetsWithDup(nums: number[]): number[][] {
 // @lcpr case=end
 
  */
-
