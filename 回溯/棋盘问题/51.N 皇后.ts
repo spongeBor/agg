082806502027d3b5f2eb9bef9c6fee224bbe1648
spongeBor <spongeBor@proton.me>
2025-05-07 10:29:07 +0800

    feat: finished 51

--- 回溯/棋盘问题/51.N 皇后.ts
+++ 回溯/棋盘问题/51.N 皇后.ts
@@ -8,11 +8,48 @@ export default {};
 
 // @lc code=start
 function solveNQueens(n: number): string[][] {
-    
-};
+  const result: string[][] = [];
+  const board: string[][] = new Array(n)
+    .fill(0)
+    .map(() => new Array(n).fill("."));
+
+  function isValid(row: number, col: number) {
+    // 检查同一列
+    for (let i = 0; i < row; i++) {
+      if (board[i][col] === "Q") return false;
+    }
+    // 检查左上角
+    for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+      if (board[i][j] === "Q") return false;
+    }
+    // 检查右上角
+    for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+      if (board[i][j] === "Q") return false;
+    }
+    return true;
+  }
+
+  function backtracking(row: number) {
+    // 终止条件
+    // 如果行数等于n，说明已经找到了一种解法，将board转换为字符串数组并加入结果集
+    if (row === n) {
+      result.push(board.map((row) => row.join("")));
+      return;
+    }
+    // 遍历列
+    for (let col = 0; col < n; col++) {
+      if (isValid(row, col)) {
+        board[row][col] = "Q";
+        backtracking(row + 1);
+        board[row][col] = ".";
+      }
+    }
+  }
+
+  backtracking(0);
+  return result;
+}
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -24,4 +61,3 @@ function solveNQueens(n: number): string[][] {
 // @lcpr case=end
 
  */
-
