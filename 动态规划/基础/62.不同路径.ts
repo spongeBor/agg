d22966ade3cca7fb58bd8298e8e003592036dc81
spongeBor <spongeBor@proton.me>
2025-05-08 14:30:43 +0800

    feat: finished 62

--- 动态规划/基础/62.不同路径.ts
+++ 动态规划/基础/62.不同路径.ts
@@ -8,11 +8,26 @@ export default {};
 
 // @lc code=start
 function uniquePaths(m: number, n: number): number {
-    
-};
+  // 动态规划
+  // dp[i][j] 是从(0,0)到(i,j)的路径数
+  // 状态转移方程：dp[i][j] = dp[i-1][j] + dp[i][j-1]
+  // 初始化：dp[0][0] = 1
+  // 返回：dp[m-1][n-1]
+  let dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  for (let i = 0; i < m; i++) {
+    dp[i][0] = 1;
+  }
+  for (let j = 0; j < n; j++) {
+    dp[0][j] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  return dp[m - 1][n - 1];
+}
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -32,4 +47,3 @@ function uniquePaths(m: number, n: number): number {
 // @lcpr case=end
 
  */
-
