1b7ddec852d88c148fb6b02d2c5825c0681158dd
spongeBor <spongeBor@proton.me>
2025-05-08 14:25:25 +0800

    feat: add 62

---
+++ 动态规划/基础/62.不同路径.ts
@@ -0,0 +1,35 @@
+export default {};
+/*
+ * @lc app=leetcode.cn id=62 lang=typescript
+ * @lcpr version=30200
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+function uniquePaths(m: number, n: number): number {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 3\n7\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 3\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 7\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 3\n3\n
+// @lcpr case=end
+
+ */
+
