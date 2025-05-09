18f30dd4871f5106c4acb6cde26fdee893412423
spongeBor <spongeBor@proton.me>
2025-05-09 11:16:08 +0800

    feat: 完成0-1背包基础问题

--- 动态规划/背包/0-1背包基础.ts
+++ 动态规划/背包/0-1背包基础.ts
@@ -3,20 +3,44 @@ export default {};
 
 function bag01(weight: number[], value: number[], bagSize: number) {
   // dp[i][j] 表示前i个物品在容量为j的背包中的最大价值
-  // dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]] + value[i])
-  // dp[0][j] = 0
-  // dp[i][0] = 0
+  // 这是我们的状态定义，i表示考虑前i个物品，j表示背包容量为j时的最大价值
+
+  // dp[i-1][j] 表示不选第i个物品
+  // 这种情况下，最大价值等于前i-1个物品在容量为j的背包中的最大价值
+
+  // dp[i-1][j-weight[i-1]] + value[i-1] 表示选第i个物品
+  // 前提是j-weight[i-1]>=0，即背包剩余容量能放下第i个物品
+  // 这种情况下，最大价值等于"前i-1个物品在剩余容量j-weight[i-1]的最大价值"加上"当前物品的价值value[i-1]"
+
+  // 状态转移方程: dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i-1]] + value[i-1])
+  // 这个方程表示我们要在"不选"和"选"两种情况中取最大值
+
+  // 初始化: dp[0][j] = 0，表示没有物品时，背包价值为0
+  // dp[i][0] = 0，表示背包容量为0时，无法放入任何物品，价值为0
+
+  // 创建二维数组dp，大小为(weight.length+1)*(bagSize+1)，初始值都为0
+  // +1是因为我们需要考虑"0个物品"和"容量为0"的情况
   let dp = new Array(weight.length + 1)
     .fill(0)
     .map(() => new Array(bagSize + 1).fill(0));
+
+  // 开始填充dp数组，i从1开始，表示考虑第1个物品
   for (let i = 1; i <= weight.length; i++) {
+    // j从0开始，表示背包容量从0开始考虑
     for (let j = 0; j <= bagSize; j++) {
+      // 如果当前背包容量j小于第i个物品的重量，那么无法选择该物品
+      // 注意：weight[i-1]表示第i个物品的重量，因为数组索引从0开始
       if (j < weight[i - 1]) {
+        // 此时只能选择不放入第i个物品，所以dp[i][j]等于dp[i-1][j]
         dp[i][j] = dp[i - 1][j];
       } else {
+        // 如果背包容量足够，我们可以选择放入或不放入第i个物品
+        // 取两种情况的最大值：
+        // 1. 不放入第i个物品：dp[i-1][j]
+        // 2. 放入第i个物品：dp[i-1][j-weight[i-1]] + value[i-1]
         dp[i][j] = Math.max(
           dp[i - 1][j],
-          dp[i - 1][j - weight[i - 1]] + value[i - 1]
+          dp[i - 1][j - weight[i - 1]] + value[i - 1] // value 从0开始，dp从1开始，所以value[i-1]是第涂个物品
         );
       }
     }
