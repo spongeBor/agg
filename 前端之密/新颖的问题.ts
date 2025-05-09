189d0e21b2aff1c53a305202114c6b92f2b32f6a
spongeBor <spongeBor@proton.me>
2025-05-09 15:45:52 +0800

    feat: 增加新颖的题的模块

---
+++ 前端之密/新颖的问题.ts
@@ -0,0 +1,30 @@
+export default {};
+// 1. 比较版本
+// 12.3.1
+// 12.7.8
+// 1.5.6-alpha.1
+// 7.2.3-beta
+
+function* walk(str: string) {
+  let part = "";
+  const terminal = [".", "-"];
+  for (let i = 0; i < str.length; i++) {
+    if (terminal.includes(str[i])) {
+      // 终结符
+      yield part;
+      part = "";
+    } else {
+      part += str[i];
+    }
+  }
+  if (part) {
+    yield part;
+  }
+}
+
+const iterator = walk("1.5.6-alpha.1");
+for (const item of iterator) {
+  console.log(item);
+}
+
+// 2.
