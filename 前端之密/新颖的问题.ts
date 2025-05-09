f851c04a31a84c6743545c5a76200a2ca24a8a47
spongeB <spongeBor@proton.me>
2025-05-09 22:44:55 +0800

    feat: 增加一些有趣的实现

--- 前端之密/新颖的问题.ts
+++ 前端之密/新颖的问题.ts
@@ -27,4 +27,49 @@ for (const item of iterator) {
   console.log(item);
 }
 
-// 2.
+// 2.闭包泄漏
+var o = (function () {
+  var obj: any = {
+    a: 1,
+    b: 2,
+  };
+  return {
+    get: function (k: any) {
+      // return obj[k];
+      // 解决
+      if (obj.hasOwnProperty(k)) {
+        return obj[k];
+      }
+      return undefined;
+    },
+  };
+})();
+//如何在不改变上面代码的情况下
+// 修改 obj 对象
+Object.defineProperty(Object.prototype, "abc", {
+  get() {
+    return this;
+  },
+});
+console.log(o.get("abc"));
+
+// 3. var [a, b] = {a: 1, b: 2}; 成立
+// 要使这个解构赋值成立，需要让对象实现迭代器协议
+Object.prototype[Symbol.iterator] = function () {
+  return Object.values(this)[Symbol.iterator]();
+};
+
+// 测试
+var [a, b] = { a: 1, b: 2 };
+console.log(a, b); // 1 2
+
+// 注意：在实际项目中不建议修改Object.prototype，这里只是为了演示
+
+// 4. 统计字符串出现的频率
+
+const strr = "fgadfsakrlewquiorewq";
+const result = strr.split("").reduce((a: any, b: any) => {
+  a[b]++ || (a[b] = 1);
+  return a;
+}, {});
+console.log(result);
