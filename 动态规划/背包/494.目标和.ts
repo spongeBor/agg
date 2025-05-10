9496b36700f3ee377dcf8121998111a539163d8a
spongeB <spongeBor@proton.me>
2025-05-10 10:13:27 +0800

    feat: add 494

---
+++ 动态规划/背包/494.目标和.ts
@@ -0,0 +1,27 @@
+export default {};
+/*
+ * @lc app=leetcode.cn id=494 lang=typescript
+ * @lcpr version=30200
+ *
+ * [494] 目标和
+ */
+
+// @lc code=start
+function findTargetSumWays(nums: number[], target: number): number {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,1,1,1,1]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n1\n
+// @lcpr case=end
+
+ */
+
