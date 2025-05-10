adedc5acc93758cd017e05153f048f7cfb77d34b
spongeB <spongeBor@proton.me>
2025-05-10 08:58:21 +0800

    feat: add 1049

---
+++ 动态规划/背包/1049.最后一块石头的重量 II.ts
@@ -0,0 +1,27 @@
+export default {};
+/*
+ * @lc app=leetcode.cn id=1049 lang=typescript
+ * @lcpr version=30200
+ *
+ * [1049] 最后一块石头的重量 II
+ */
+
+// @lc code=start
+function lastStoneWeightII(stones: number[]): number {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [2,7,4,1,8,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [31,26,33,21,40]\n
+// @lcpr case=end
+
+ */
+
